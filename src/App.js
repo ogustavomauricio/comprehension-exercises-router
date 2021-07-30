2f0093d21ca8d5f65f5e2708096dc39c1c35f4a7
Gustavo Mauricio <gmauricio207@gmail.com>
2021-07-30 15:24:32 -0300

    Req do 1 ao 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import About from './components/About'
+import Users from "./components/Users"
+import { Link } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+      <Link clasName = "nav" to="/">Home</Link>
+      <Link clasName = "nav" to="/About">About</Link>
+      <Link clasName = "nav" to="Users">Users</Link>
+        <Route path="/About" component={About} />
+        <Route path="/Users" component={Users} />
+        <Route exact path="/" component={Home} />
+      </BrowserRouter>  
     );
   }
 }
